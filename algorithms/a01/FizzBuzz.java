f0f2610b32e201f2691aeacfa6e8e4a97f05451d
thitiphongD <80618380+thitiphongD@users.noreply.github.com>
2022-05-09 20:49:00 +0700

    Create FizzBuzz.java

---
+++ algorithms/a01/FizzBuzz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class FizzBuzz {
+	public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Input : ");
+      int n = input.nextInt();
+      for (int i = 0; i <= n; i++) {
+        System.out.println(i % 15 != 0 ? i % 5 != 0 ? i % 3 != 0 ? i : "Fizz" : "Buzz" : "FizzBuzz");
+      }
+    }
+	}
+}
